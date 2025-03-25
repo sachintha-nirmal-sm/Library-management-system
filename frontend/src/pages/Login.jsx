58172b5fbcebf6707646d2e039df9acafccaa20a
Sachintha Nirmal <s.nirmal.sm@gmail.com>
2025-03-25 21:08:10 +0530

    update css and jsx of login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <div className="login-form">
+      <div className="login-form" style={{ flexGrow: 1 }}>
         <h2>Login</h2>
         <form>
           <div className="form-group">
@@ -27,6 +27,7 @@ const Login = () => {
         <p className="forgot-password">Forgot password?</p>
         <p className="signup-link" onClick={handleSignupClick}>Not a member? Signup now</p>
       </div>
+
     </div>
   );
 };
